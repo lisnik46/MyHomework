6dcc3bb86d70dcd61f2977a78e59ae26d9be6076
lisnik46 <lisnik46@gmail.com>
2025-03-05 13:12:08 +0300

    Added new feature in my_feature_branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from testBranch!");
+        System.out.println("Hello, Git with new feature!");
     }
 }
